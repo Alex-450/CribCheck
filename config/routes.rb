28a612a8f93d9c5165af09d9e59b0d4892d74b9b
Victoria Shanukova <vshanukova@gmail.com>
2021-08-30 20:55:23 +0100

    fixed created method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :properties
-=======
   resources :landlords, only: [:create, :new, :show]
->>>>>>> de56b782502ce055d9122404167081fcb6d46b55
 end
