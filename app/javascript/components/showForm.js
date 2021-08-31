89ccc71447b33027cfe60aed54422d93e687c124
Alex Stearn <alex.wmstearn@gmail.com>
2021-08-31 18:19:06 +0200

    fix js file

--- app/javascript/components/showForm.js
+++ app/javascript/components/showForm.js
@@ -1,14 +1,16 @@
 const button = document.getElementById('showHide')
+const landlordForm = document.getElementById("landlordForm")
+const propertyForm = document.getElementById("propertyForm")
 
 const showForm = () => {
   button.addEventListener('click', (event) => {
-    var x = document.getElementById("landlordForm")
-  if (x.style.display === "none") {
-      x.style.display = "block";
+  if (landlordForm.style.display === "none") {
+      landlordForm.style.display = "block";
+      propertyForm.style.display = "none"
+      button.style.display = "none";
     } else {
-      x.style.display = "none";
+      landlordForm.style.display = "none";
     }
-  button.style.display = "none";
 })}
 
 export { showForm }
