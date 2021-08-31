215b588e4ab920f54387c66d3f951a6a0c1014bd
Victoria Shanukova <vshanukova@gmail.com>
2021-08-31 13:05:20 +0100

    added map to property index

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,7 @@ class Property < ApplicationRecord
   belongs_to :user
   belongs_to :landlord
   has_many :reviews
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
