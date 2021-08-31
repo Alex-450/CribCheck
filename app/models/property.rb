36572c3fa1a12b2cbc56732759b54d2927ba4780
Victoria Shanukova <vshanukova@gmail.com>
2021-08-31 13:55:53 +0100

    added Pg Search to prop model

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,9 +2,13 @@ class Property < ApplicationRecord
   belongs_to :user
   belongs_to :landlord
   has_many :reviews
+  validates_presence_of :address, :user_id, :landlord_id
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  
-  validates_presence_of :address, :user_id, :landlord_id
+
+  include PgSearch::Model
+  pg_search_scope :search_by_address,
+    against: :address,
+    using: { tsearch: { prefix: true } }
 end
