0cfc1877b9689d75360d12584e619a8814e3dc58
Mick Butler <80526334+mickbut-ler@users.noreply.github.com>
2021-09-02 13:58:05 +0200

    form review frontend

---
+++ app/javascript/controllers/range-slider-landlord_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ 'range', 'hidden' ]
+
+
+
+  connect() {
+
+
+    console.log(this.rangeTarget)
+    console.log(this.hiddenTarget)
+
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+  }
+  updateValue(e) {
+    // this.updateText(e.currentTarget.value); t
+    this.hiddenTarget.value = e.currentTarget.value;
+  }
+}
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   has_many :chatrooms
   has_one :landlord, through: :properties
   has_many_attached :photos
+  validates :landlord_rating, inclusion: { in: 0..5 }
 end
