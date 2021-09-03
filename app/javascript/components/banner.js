b32559b11d54c0b49a39f21f2f029d9fe29ec809
Alex Stearn <53116798+Alex-450@users.noreply.github.com>
2021-09-03 15:24:28 +0200

    fix typo

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Check out your future appartment"],
+    strings: ["Check out your future apartment"],
     typeSpeed: 50,
     loop: false
   });
