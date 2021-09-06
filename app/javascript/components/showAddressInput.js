7d4fd7fba8fae15329035beae95380a81562b25d
Alex Stearn <alex.wmstearn@gmail.com>
2021-09-06 10:52:49 +0200

    fix address form show function

--- app/javascript/components/showAddressInput.js
+++ app/javascript/components/showAddressInput.js
@@ -2,9 +2,14 @@ const showAddressInput = () => {
   const addressInput = document.getElementById('propertyAddress');
   const createLandlordButton = document.getElementById('showLandlordForm')
   if (addressInput && createLandlordButton) {
-    if ($('.select2Landloed').val !== "") {
+    $('.select2Landlord').on('change', function() {
+      if ($('.select2Landlord').val() !== "") {
+      console.log($('.select2Landlord').val())
       addressInput.style.display = "block";
       createLandlordButton.style.display = "none";
-}}};
+    }
+    })
+  }
+};
 
 export { showAddressInput }
